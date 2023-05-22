0d51b1174df75f5aea8e0b9013234622fa57d0c5
Mateusz Chłopek <matikc25@gmail.com>
2023-05-23 00:18:32 +0200

    Init commit - cmake project, SFML
    Created a cmake project, added SFML library.

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include "SFML/Graphics.hpp"
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(sf::Vector2u(200, 200)), "SFML works!");
+    sf::CircleShape shape(100.0f);
+
+    shape.setFillColor(sf::Color::Yellow);
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if(event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+    
+    std::cout << "Hello there!\n";
+
+	return 0;
+}
